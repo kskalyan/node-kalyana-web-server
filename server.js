f719aa553717ea8e8c47bd8b9604d8076de0fcda
501626442 <KSKalyana.Chakravarthy@ge.com>
2018-04-26 13:42:46 -0400

    added new module called projects

--- server.js
+++ server.js
@@ -51,6 +51,14 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Project Page',
+        welcomeMessage: 'We are in Project Page'
+    });
+});
+
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Uable to handle request'
